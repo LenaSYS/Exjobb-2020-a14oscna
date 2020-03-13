e0379265ee57b3e448fc609faa3c1bad59287a39
a14oscna <a14oscna@student.his.se>
2020-03-13 12:24:10 +0100

    added a service

--- Angular/main.js
+++ Angular/main.js
@@ -1,5 +1,24 @@
 angular.module('stockTable', [])
-  .controller('tableController', function($scope, $rootScope) {
+  .factory('dataService', function(){
+    var tableBodyData = null;
+    var tableHeadData = null;
+    return {
+      setHeadData: function(dataObj){
+        tableHeadData = dataObj
+      },
+      getHeadData: function(){
+        return tableHeadData;
+      },
+      setBodyData: function(dataObj){
+        tableBodyData = dataObj
+      },
+      getBodyData: function(){
+        return tableBodyData;
+      }
+    };
+  })
+
+  .controller('tableController', function($scope, $rootScope, dataService) {
     $rootScope.frameworkName = 'AngularJS';
     $scope.content = [
       {date:'2020-03-10', name:'Volvo', value: 50},
@@ -7,21 +26,25 @@ angular.module('stockTable', [])
       {date:'2020-03-13', name:'Jeep', value: 320},
       {date:'2020-03-19', name:'Fiat', value: 500},
       {date:'2020-03-10', name:'Tesla', value: 1000}];
+
+      $scope.thead = dataService.getHeadData();
+      console.log($scope.thead)
+      
   })
   
-  .controller('fileController', function($scope, $rootScope) {
+  .controller('fileController', function($scope, dataService) {
     $scope.upload = function(file) {
       var file = file.files[0];
       var reader = new FileReader();
       if (file) {
         reader.readAsText(file);
         reader.onload = function(e) {
-        var csv = e.target.result;
-        var data = Papa.parse(csv, {header : true, encoding: "utf-8", preview: 10});
-        
-          $rootScope.datasetBody = data.data;
-          $rootScope.datasetHead = data.meta.fields;
-          console.log($rootScope.datasetHead)
+          var csv = e.target.result;
+          var data = Papa.parse(csv, {header : true, encoding: "utf-8", preview: 10});
+          
+          dataService.setHeadData(data.meta.fields);
+          dataService.setHeadData(data.data);
+          console.log(dataService.getBodyData())
         };
       }
     }
