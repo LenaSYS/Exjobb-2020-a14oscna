da3000227bac0d1f8158ddaad97808b5046a6b38
a14oscna <a14oscna@student.his.se>
2020-03-16 00:58:04 +0100

    changed to a custom directive, properly updates table now

--- Angular/main.js
+++ Angular/main.js
@@ -1,24 +1,25 @@
 angular.module('stockTable', [])
-  .controller('tableController', function($scope) {
-    $scope.frameworkName = 'AngularJS'; //will remove later, was only for testing purposes
-      $scope.upload = function(file) {
-        var file = file.files[0];
+
+.directive("selectNgFile", function() {
+  return {
+    require: "ngModel",
+    link: function(scope,elem,attrs,ngModel) {
+      elem.on("change", function(e) {
+        var rowSize = 100;
+        var file = elem[0].files[0];
         var reader = new FileReader();
         if (file) {
           reader.readAsText(file);
           reader.onload = function(e) {
             var csv = e.target.result;
-            var data = Papa.parse(csv, {header : true, encoding: "utf-8", preview: 10});
-            $scope.dataHead = data.meta.fields;
-            $scope.dataBody = data.data;
+            var data = Papa.parse(csv, {header : true, preview: rowSize});
+            scope.stockHead = data.meta.fields
+            ngModel.$setViewValue(data.data);
           };
         }
-      }
-
-      $scope.updateTable = function() {
-        $scope.thead = $scope.dataHead;
-        $scope.tbody = $scope.dataBody;
-        console.log($scope.tbody);
-        console.log($scope.thead);
-      };      
-  });
+      })
+    }
+  }
+})
+.controller('tableController', function($scope) {    
+});
