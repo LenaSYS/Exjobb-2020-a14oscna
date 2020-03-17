e888c0eba23fe5e0e0bb3d477288b0e74382a660
a14oscna <a14oscna@student.his.se>
2020-03-17 20:09:00 +0100

    added sort and filter, changed to float where needed

--- Angular/main.js
+++ Angular/main.js
@@ -5,7 +5,7 @@ angular.module('stockTable', [])
     require: "ngModel",
     link: function(scope,elem,attrs,ngModel) {
       elem.on("change", function(e) {
-        var rowSize = 100;
+        var rowSize = 10;
         var file = elem[0].files[0];
         var reader = new FileReader();
         if (file) {
@@ -13,9 +13,20 @@ angular.module('stockTable', [])
           reader.onload = function(e) {
             var csv = e.target.result;
             var data = Papa.parse(csv, {header : true, preview: rowSize});
-            data.meta.fields.unshift("Index");
+            data.meta.fields.unshift("Index"); //will be removed later
+
+            data.data.forEach(x => {
+              x.Kursutveckling = scope.parseNumbers(x.Kursutveckling);
+              x.Direktavkastning = scope.parseNumbers(x.Direktavkastning);
+              x.PE = scope.parseNumbers(x.PE);
+              x.PS = scope.parseNumbers(x.PS);
+              x.PB = scope.parseNumbers(x.PB);
+            });
+
             scope.stockHead = data.meta.fields;
             ngModel.$setViewValue(data.data);
+            console.log(scope);
+            console.log(data.data);
           };
         }
       })
@@ -23,4 +34,17 @@ angular.module('stockTable', [])
   }
 })
 .controller('tableController', function($scope) {    
+  $scope.sortedColumn = "Bolagsnamn";
+  $scope.reverseOrder = false;
+
+  $scope.parseNumbers = function (s) {
+    if (s == "") return s;
+    return parseFloat(s);
+  };
+
+  $scope.sortData = function (column) {
+    $scope.reverseOrder = ($scope.sortedColumn == column) ? !$scope.reverseOrder : false;
+    $scope.sortedColumn = column;
+    console.log(column);
+  };
 });
